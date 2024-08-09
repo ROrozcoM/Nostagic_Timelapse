21dabfe64c51b3cb558eff5f11ceed0ee416a9fe
ROrozcoM <rafozco@gmail.com>
2024-08-09 13:10:31 +0200

    latest issues

--- src/callbacks.py
+++ src/callbacks.py
@@ -1,7 +1,12 @@
 from dash import html, callback, Output, Input, dcc, State, no_update, ctx
 import dash_mantine_components as dmc
 from dash.exceptions import PreventUpdate
-from .components import generate_map
+from .components import generate_map, generate_images
+import dash_bootstrap_components as dbc
+from shapely.geometry import shape
+import json
+from dash.exceptions import PreventUpdate
+from pyproj import Transformer
 
 
 
@@ -26,18 +31,3 @@ def generate_layout(pathname):
     ]
     return layout
 
-@callback(
-    Output('farm-marker', 'position'),
-    Output("button-submit", "disabled"),
-    Input('farm-selector', 'n_clicks'),
-    State('farm-selector', 'clickData'),
-    prevent_initial_call=True,
-)
-def update_marker(n_clicks, click_data):
-    print(n_clicks, click_data)
-    if n_clicks is None:
-        # Prevent initial callback invocation
-        raise PreventUpdate
-
-    # Get the click event data
-    return {'lat': click_data['latlng']['lat'], 'lng': click_data['latlng']['lng']}, False
--- src/main.py
+++ src/main.py
@@ -148,22 +148,6 @@ app.layout = dmc.MantineProvider(
     forceColorScheme="light",
 )
 
-# @callback(
-#     Output('farm-marker', 'position'),
-#     Output("button-submit", "disabled"),
-#     Input('farm-selector', 'n_clicks'),
-#     State('farm-selector', 'clickData'),
-#     prevent_initial_call=True,
-# )
-# def update_marker(n_clicks, click_data):
-#     print(n_clicks, click_data)
-#     if n_clicks is None:
-#         # Prevent initial callback invocation
-#         raise PreventUpdate
-#
-#     # Get the click event data
-#     return {'lat': click_data['latlng']['lat'], 'lng': click_data['latlng']['lng']}, False
-
 @callback(
     Output("button-submit-sector", "disabled"),
     Output("new_sector-template", "data"),
